8c5adf27c2f6e541a88d6ef9414319f6b9de06b0
Austin Caron <aus10caron@icloud.com>
2019-06-10 23:28:44 -0400

    View + Delete Employee

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,11 +6,10 @@
       </li>
       <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
         <div class="chip">{{employee.dept}}</div>
-
-        {{employee.employee_id}}:{{employee.name}}
+        {{employee.employee_id}}: {{employee.name}}
         <router-link
           class="secondary-content"
-          v-bind:to="{name: 'view-employee, params: {employee_id: employee.employee_id'}"
+          v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}"
         >
           <i class="fa fa-eye"></i>
         </router-link>
